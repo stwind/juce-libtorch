664add66a6431a4fd626beb2f130b284d98f45e0
stwind <stwindfy@gmail.com>
2023-02-25 21:29:21 +0800

    [chore] Add JUCE

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <juce_core/juce_core.h>
 
-int main() {
-  std::cout << "Hello World!" << std::endl;
+int main(int argc, char* argv[])
+{
+
+  juce::ignoreUnused(argc, argv);
+  std::cout << "Hello JUCE!" << std::endl;
+
   return 0;
 }
